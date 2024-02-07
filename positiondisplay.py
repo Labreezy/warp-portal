0ae8b024cc5c56deb849019b7c67a8f841beb6f3
Ethan Robards <yourtooslow@gmail.com>
2024-02-07 16:53:46 -0600

    it works omgomgomgomg

--- positiondisplay.py
+++ positiondisplay.py
@@ -1,23 +1,73 @@
+import threading
+import tkinter.messagebox
+from datetime import datetime
+import time
 from tkinter import *
 from tkinter import ttk
 import struct, sys
 import frida
 from time import sleep
+from threading import Thread
 
-float3format = "X: {} Y: {} Z: {}"
+float3format = "X: {:.02f} Y: {:.02f} Z: {:.02f}"
 positionVar = None
+ptr_val = -1
+hasError = False
+curr_pos = (0,0,0)
+saved_pos = (0,0,0)
+recv_time = time.time()
 
+def update_position():
 
+    while ptr_val > 0:
+        script.post({"type": "readpos", "payload": ptr_val})
+        sleep(0.3)
+    print("update_position returned")
+    print(f"HasError: {hasError}")
+    print(f"Ptr Val: {ptr_val:X}")
+
+def save_position():
+    global saved_pos
+    saved_pos = curr_pos
+    print(f"saved position as {saved_pos}")
+
+def load_position():
+    global saved_pos
+    print(f"LOADING {saved_pos}")
+    if saved_pos[0] == 0 and saved_pos[1] == 0 and saved_pos[2] == 0:
+        return
+    payload_obj = {"pos_bytes": list(struct.pack(">3f",*saved_pos)), "baseptr": ptr_val}
+    script.post({"type":"writepos","payload": payload_obj})
+
+posThread = Thread(target=update_position,daemon=True)
 
 def on_message(message, data):
+    global hasError, ptr_val, posThread, pos, recv_time, curr_pos
     if message['type'] == 'send':
-        print(message['payload'])
+        #   print(message['payload'])
         if message['payload'].startswith("0x"):
-            ptr_val = int(message['payload'],16)
-            sleep(2)
-            script.post({"type": "findptr", "payload": ptr_val})
+            now = datetime.now()
+            cooldown_tdelta = time.time() - recv_time
+            if ptr_val == -1 or cooldown_tdelta >= 5:
+                ptr_val = int(message['payload'],16)
+                sleep(.75) #wait a bit for 06 to load
+                recv_time = time.time()
+                hasError = False
+                #script.post({"type": "checkamigo", "payload": ptr_val})
+                script.post({"type": "readpos", "payload": ptr_val})
+                if not posThread.is_alive():
+                    posThread = Thread(target=update_position,daemon=True)
+                    posThread.start()
+            elif cooldown_tdelta < 5:
+                print("too soon bro")
+        elif message['payload'] == "position":
+            floatarr = data
+            curr_pos = struct.unpack(">3f", floatarr)
+            positionVar.set(float3format.format(*curr_pos))
     elif message['type'] == 'error':
         print(message['stack'])
+        hasError = True
+        script.post("rescan")
 
 if __name__ == '__main__':
     session = frida.attach("xenia.exe")
@@ -25,8 +75,11 @@ if __name__ == '__main__':
     script.on('message', on_message)
     script.load()
     root = Tk()
+    root.title("Warp Portal v0.0.1 alpha ver.")
     positionVar = StringVar()
     positionVar.set(float3format.format(0, 0, 0))
-    l = ttk.Label(textvariable=positionVar)
-    l.pack()
+    l = ttk.Label(textvariable=positionVar).grid(row=0,column=0,columnspan=2)
+    savebtn = ttk.Button(text="Save",command=save_position).grid(row=1,column=0)
+    loadbtn = ttk.Button(text="Load",command=load_position).grid(row=1,column=1)
+
     root.mainloop()
--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var exec_base = ptr(0xA0000000);
 var exec_size = 0x1000000;
-var instr_addr;
+var instr_addr = ptr(0);
 var xenia_offset = 0x100000000;
-var postureControlOffsets = [0x50, 0x50, 0xDC];
+
 var amigoOffsets = [0x50, 0x50, 0x140]; //after, 0x24 != 0 is player
 var playerObjPtrs = [];
+var r4vals = [];
 var playeridx = -1;
+var found_intercept = false;
 console.log("Starting");
 
 function swap32(val) {
@@ -14,34 +16,60 @@ function swap32(val) {
            | ((val >> 8) & 0xFF00)
            | ((val >> 24) & 0xFF);
 }
-function onPtrRecv(val){
-    var pointer = ptr(val.payload);
-    console.log("Received " + pointer);
-    var curr_ptr = pointer;
-    for(var i = 1; i < postureControlOffsets.length; i++) {
-        var be_val = curr_ptr.add(postureControlOffsets[i]).readU32();
-        curr_ptr = ptr(swap32(be_val)).add(xenia_offset);
-        console.log("level " + i + ": " + curr_ptr);
-    }
+
+var posbuf = new ArrayBuffer(12);
+var posview = new DataView(posbuf);
+
+function readposition(msg){
+    var baseptr = ptr(msg.payload);
+    var posptr = ptrtoPosition(baseptr);
+    var posbytes = posptr.readByteArray(12);
+    posview = new DataView(posbytes);
+    send("position", posbytes)
+}
+function writeposition(msg){
+    console.log("guh");
+    var payloadobj = msg.payload;
+    console.log(JSON.stringify(payloadobj));
+    var posbytes = payloadobj.pos_bytes;
+    var posptr = ptrtoPosition(ptr(payloadobj.baseptr))
+        posptr.writeByteArray(posbytes);
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function scanForPosition() {
+//mov eax, 821E8C50 is the pattern but you have to wait for the code to be JITed first
+    return Memory.scanSync(exec_base, exec_size, "B8 50 8C 1E 82");
+
 }
-//mov eax, 821E8C50 is the pattern
-Memory.scan(exec_base, exec_size,"B8 50 8C 1E 82" ,{onMatch(address, size) {
-    console.log("Match found at "+ address);
-    instr_addr = address;
-    Interceptor.attach(instr_addr, {
+
+function ptrtoPosition(base){
+    var r4ptr = ptr(swap32(base.add(xenia_offset + 0xDC).readU32())).add(xenia_offset); //[r4+DC]
+    var writeable_position = r4ptr.add(0xB0); //[r4 + DC]+B0
+    return writeable_position
+}
+
+var addresses = scanForPosition();
+console.log("Done Scanning");
+console.log(JSON.stringify(addresses))
+if(addresses.length > 0) {
+    found_intercept = true;
+    console.log("Attaching at " + addresses[0].address);
+    Interceptor.attach(addresses[0].address, {
         onEnter(args) {
-            var base = this.context.rsi.add(0x48).readPointer(); //r5
-            console.log("Base: " + base);
-            var r5ptr = ptr(swap32(base.add(xenia_offset).readU32())).add(xenia_offset+0x50);
-            send(r5ptr)
+            var base = this.context.rsi.add(0x40).readPointer(); //r4
+            console.log("R4: " + base);
+            r4vals.push(base);
+
+                send(base);
+
+
         }
     });
-    return 'stop';
-}});
-recv('findptr',val => onPtrRecv(val))
+}
+var interval_pos = setInterval(() => recv("readpos", msg => readposition(msg)), 350);
+var interval_rescan = setInterval(() => recv("rescan", msg => scanForPosition()),350);
+var interval_write = setInterval( ()=>recv("writepos", msg => writeposition(msg)), 350)
+
+
 
 
